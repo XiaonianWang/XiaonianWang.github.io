8289bb0413da05c5a32fa38c68a89a175c54940a
王小年 <wxnfive@gmail.com>
2026-04-21 21:09:06 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// 页面滚动平滑效果
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
